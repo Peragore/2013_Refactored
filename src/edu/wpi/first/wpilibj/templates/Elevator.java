82ce8c408817067066dd19d2d920606e216366f6
Philip Mulford <peragorefalith1@gmail.com>
2013-02-12 15:37:03 -0500

    Revert "Revert "elevator works!""
    This reverts commit d0c1007e131cec7517cc1a25e64bf42867578db6.

--- src/edu/wpi/first/wpilibj/templates/Elevator.java
+++ src/edu/wpi/first/wpilibj/templates/Elevator.java
@@ -12,27 +12,23 @@ import edu.wpi.first.wpilibj.Talon;
  * @author Jamie
  */
 public class Elevator {
-    Talon elevatorTalon2 = new Talon(1, 4);
-    Talon elevatorTalon1 = new Talon(1, 5);
+    
+    Talon elevatorTalons = new Talon(1, 7);
     AnalogChannel angleMeter = new AnalogChannel(5);
     
     double minLimit = 0;
     double maxLimit = 5;
-    double currentAngle;
+    double currentAngle; //= angleMeter.getVoltage();
     
     public void raise(){
-        elevatorTalon1.set(0.1);
-        elevatorTalon2.set(0.1);
+        elevatorTalons.set(0.1);
     }
     
     public void lower(){
-        elevatorTalon1.set(-0.1);
-        elevatorTalon2.set(-0.1);
+        elevatorTalons.set(-0.1);
     }
-    public void stop(){
-        elevatorTalon1.set(0);
-        elevatorTalon2.set(0);
-    
+    public void off(){
+        elevatorTalons.set(0);
     }
     
     public void goToAngle(double targetAngle){
@@ -40,13 +36,11 @@ public class Elevator {
         //TODO: make some sort of conversion from voltage to angle
         if (targetAngle < maxLimit && targetAngle > minLimit){
             currentAngle = angleMeter.getVoltage();
-            if (Math.abs(targetAngle - currentAngle) <= .1){
-              stop();
-            }
-            if (targetAngle > currentAngle){
+            if (Math.abs(targetAngle - currentAngle) <= .1){//TODO: check angle
+              off();
+            } else if (targetAngle > currentAngle){
                 raise();
-            }
-            if (targetAngle < currentAngle){
+            } else{
                 lower();
             }
              
